6b4d2db37758d576357885bb301df99194c9c8df
Bill <abcde5676151199@gmail.com>
2021-10-08 18:18:50 +0800

    eventlistenser

--- script.js
+++ script.js
@@ -19,3 +19,20 @@ while(password != input && !out_of_limit){
 if(out_of_limit){
     alert("超出輸入次數")
 }else{alert("登入成功")}
+
+//eventlistenser
+
+var btn = document.getElementById("btn")
+btn.addEventListener("click", function(){
+    alert("按屁阿")
+    this.innerText = "按屁"
+    this.style.color = "red"   
+})
+
+var img = document.getElementById("img")
+img.addEventListener("mouseover", function(){
+    this.src = "亭慧2.jpeg"
+})
+img.addEventListener("mouseout", function(){
+    this.src = "亭慧.jpeg"
+})
